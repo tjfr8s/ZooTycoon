a89a1f818c8d52d71d428ee8ccfd0bae33271dd1
tyler <tjfr8s@gmail.com>
2018-07-14 16:35:14 -0700

    Implement Zoo constructor and destructor to handle memory management for zoo animal arrays.

--- Zoo.cpp
+++ Zoo.cpp
@@ -1,3 +1,99 @@
 #include "Zoo.hpp"
 
+/*******************************************************************************
+ * Description: Constructor for Zoo class with default parameters for m_money
+ * and the size of each animal array.
+ *
+ * Preconditions:
+ *  - Provide arguments for m_money, m_maxTigers, m_maxPenguins, 
+ *  and m_maxTurtles (or user default values).
+ *
+ * Postconditions:
+ *   - Constructs Zoo object with following properties
+ *    - m_money, m_maxTigers, m_maxPenguins, and m_maxTurtles are initialized to
+ *    argument values.
+ *    - Animal pointer members are assigned arrays of size specified in their
+ *    maxAnimalType varibles.
+ *    - m_numAnimals variables are initialized to 0
+ *    - m_isBankrupt is initialized to false
+ *    - m_boonProfit is initialized to 0
+ *******************************************************************************/
+Zoo::Zoo(double money,
+         int maxTigers,
+         int maxPenguins,
+         int maxTurtles) 
+    : m_money(money),
+      m_maxTigers(maxTigers),
+      m_maxPenguins(maxPenguins),
+      m_maxTurtles(maxTurtles),
+      m_numTigers(0),
+      m_numPenguins(0),
+      m_numTurtles(0),
+      m_isBankrupt(false),
+      m_boomProfit(0)
+{
+    // Create array of tiger pointers.
+    m_tigers = new Tiger*[m_maxTigers];
+    //
+    // Initialize Tiger*s to nullptr
+    for(int i = 0; i < m_maxTigers; i++)
+    {
+        m_tigers[i] = nullptr;
+    }
+
+    // Create array of penguin pointers.
+    m_penguins = new Penguin*[m_maxTigers];
+    //
+    // Initialize Penguin*s to nullptr
+    for(int i = 0; i < m_maxPenguins; i++)
+    {
+        m_penguins[i] = nullptr;
+    }
+
+    // Create array of turtle pointers.
+    m_turtles = new Turtle*[m_maxTigers];
+    //
+    // Initialize Turtle*s to nullptr
+    for(int i = 0; i < m_maxTurtles; i++)
+    {
+        m_turtles[i] = nullptr;
+    }
+}
+
+Zoo::~Zoo()
+{
+    // Deallocate Tiger objects in array
+    for(int i = 0; i < m_maxTigers; i++)
+    {
+        if(m_tigers[i] != nullptr)
+        {
+            delete m_tigers[i];
+        }
+    }
+    // Deallocate Tiger array
+    delete [] m_tigers;
+
+    // Deallocate Penguin objects in array
+    for(int i = 0; i < m_maxPenguins; i++)
+    {
+        if(m_penguins[i] != nullptr)
+        {
+            delete m_penguins[i];
+        }
+    }
+    // Deallocate Penguin array
+    delete [] m_penguins;
+
+    // Deallocate Turtle objects in array
+    for(int i = 0; i < m_maxTurtles; i++)
+    {
+        if(m_turtles[i] != nullptr)
+        {
+            delete m_turtles[i];
+        }
+    }
+    // Deallocate Turtle array
+    delete [] m_turtles;
+}
+
 
--- Zoo.hpp
+++ Zoo.hpp
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Author: Tyler Freitas
+ * Date: 20180713
+ * Description: This class describes a Zoo. It has data members for keeping 
+ * track of each type of animal (Tiger, Penguin, and Turtle), the currnt number
+ * or each animal, and the current size of the array's holding the animals. It
+ * also has a flag for documenting if the Zoo goes bankrupt and a data member 
+ * for holding any profit gained during a "boom". The Zoo has member functions 
+ * for simulating a day, aging animals, paying for food, causing a random event,
+ * calculating profit, purchasing adult animals, sickness events, boom events,
+ * baby events, and purchasing individual animals.
+*******************************************************************************/
 #ifndef ZOO_HPP
 #define ZOO_HPP
 
@@ -11,9 +23,9 @@ class Zoo
 {
 private:
     double m_money;
-    Tiger* m_tigers;
-    Penguin* m_penguins;
-    Turtle* m_turtles;
+    Tiger** m_tigers;
+    Penguin** m_penguins;
+    Turtle** m_turtles;
     
     int m_maxTigers;    
     int m_maxPenguins;    
@@ -26,10 +38,42 @@ private:
     bool m_isBankrupt;
     double m_boomProfit;
 public:
+/*******************************************************************************
+ * Description: Constructor for Zoo class with default parameters for m_money
+ * and the size of each animal array.
+ *
+ * Preconditions:
+ *  - Provide arguments for m_money, m_maxTigers, m_maxPenguins, 
+ *  and m_maxTurtles (or user default values).
+ *
+ * Postconditions:
+ *   - Constructs Zoo object with following properties
+ *    - m_money, m_maxTigers, m_maxPenguins, and m_maxTurtles are initialized to
+ *    argument values.
+ *    - Animal pointer members are assigned arrays of size specified in their
+ *    maxAnimalType varibles.
+ *    - m_numAnimals variables are initialized to 0
+ *    - m_isBankrupt is initialized to false
+ *    - m_boonProfit is initialized to 0
+ *******************************************************************************/
     Zoo(double m_money = 100000,
         int m_maxTigers = 10,
         int m_maxPenguins = 10,
         int m_maxTurtles = 10);
+
+/*******************************************************************************
+ * Description: Destructor that frees memory for Dynamic array of Animal 
+ * pointers.
+ *
+ * Precondition:
+ *  - Zoo object is destroyed
+ *
+ * Postconditions:
+ *  - each element in Tiger**, Penguin**, and Turtle** is deallocated (if it
+ *  isn't assigned nullptr.
+ *  - The Tiger**, Penguin**, and Turtle** arrays are freed.
+*******************************************************************************/ 
+    virtual ~Zoo();
 
 };
 
