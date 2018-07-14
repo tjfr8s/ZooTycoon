6ea7e50f0d2f92c4d9b5f7013d2d6397461abea9
tyler <tjfr8s@gmail.com>
2018-07-14 15:49:24 -0700

    Create header, implementaiton, and test files for Zoo class.

---
+++ Zoo.hpp
@@ -0,0 +1,15 @@
+#ifndef ZOO_HPP
+#define ZOO_HPP
+
+#include <cstdlib>
+#include "Animal.hpp"
+#include "Tiger.hpp"
+#include "Penguin.hpp"
+#include "Turtle.hpp"
+
+class Zoo
+{
+    
+};
+
+#endif
