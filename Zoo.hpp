5bad9c82bc79d9983aef091a3b166be27b23388c
tyler <tjfr8s@gmail.com>
2018-07-14 16:06:56 -0700

    Add data members and constructor prototype with default parameters to Zoo class.

--- Zoo.hpp
+++ Zoo.hpp
@@ -9,7 +9,28 @@
 
 class Zoo
 {
+private:
+    double m_money;
+    Tiger* m_tigers;
+    Penguin* m_penguins;
+    Turtle* m_turtles;
     
+    int m_maxTigers;    
+    int m_maxPenguins;    
+    int m_maxTurtles;    
+
+    int m_numTigers;    
+    int m_numPenguins;    
+    int m_numTurtles;    
+
+    bool m_isBankrupt;
+    double m_boomProfit;
+public:
+    Zoo(double m_money = 100000,
+        int m_maxTigers = 10,
+        int m_maxPenguins = 10,
+        int m_maxTurtles = 10);
+
 };
 
 #endif
