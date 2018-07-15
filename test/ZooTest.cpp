41cb2c9d49e008f13973e10b65dffd396e28376f
tyler <tjfr8s@gmail.com>
2018-07-14 17:32:06 -0700

    Implement test for Zoo::resizeTiger() and Zoo::addTiger().

--- test/ZooTest.cpp
+++ test/ZooTest.cpp
@@ -5,5 +5,11 @@ int main()
 {
     Zoo zoo;
     std::cout << zoo;
+    // Test addTiger() and resizeTiger()
+    for(int i = 0; i < 11; i++)
+    {
+        zoo.addTiger();
+        std::cout << zoo << "\n******************\n";
+    }
     return 0;
 }
