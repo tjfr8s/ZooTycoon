0880e6d11f866a546ad94d34f49bf67e429282f3
tyler <tjfr8s@gmail.com>
2018-07-15 08:27:49 -0700

    Test add and resize functions for turtles and penguins.

--- test/ZooTest.cpp
+++ test/ZooTest.cpp
@@ -11,5 +11,19 @@ int main()
         zoo.addTiger();
         std::cout << zoo << "\n******************\n";
     }
+
+    // Test addPenguin() and resizePenguin()
+    for(int i = 0; i < 11; i++)
+    {
+        zoo.addPenguin();
+        std::cout << zoo << "\n******************\n";
+    }
+
+    // Test addTurtle() and resizeTurtle()
+    for(int i = 0; i < 11; i++)
+    {
+        zoo.addTurtle();
+        std::cout << zoo << "\n******************\n";
+    }
     return 0;
 }
